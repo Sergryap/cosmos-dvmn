b8bbac0ca89061ddd5c74a52a92370b799e3d79a
sergryap <rs1180@mail.ru>
2022-12-23 21:31:28 +0500

    The draw function has been modified

--- main.py
+++ main.py
@@ -1,35 +1,62 @@
 import time
 import curses
+import asyncio
 
 
 def draw(canvas):
-    row, column = (5, 20)
-    canvas.addstr(row, column, 'Hello, World!')
-    canvas.border()
-    canvas.refresh()
-    time.sleep(2)
-
-
-def star(canvas):
     curses.curs_set(False)
     row, column = (5, 20)
     canvas.border()
+    coroutine = blink(canvas, row, column)
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(2)
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(0.3)
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(0.5)
+    coroutine.send(None)
+    canvas.refresh()
+    time.sleep(0.3)
+
+
+# def star(canvas):
+#     curses.curs_set(False)
+#     row, column = (5, 20)
+#     canvas.border()
+#     while True:
+#         canvas.addstr(row, column, '*', curses.A_DIM)
+#         canvas.refresh()
+#         time.sleep(2)
+#         canvas.addstr(row, column, '*')
+#         canvas.refresh()
+#         time.sleep(0.3)
+#         canvas.addstr(row, column, '*', curses.A_BOLD)
+#         canvas.refresh()
+#         time.sleep(0.5)
+#         canvas.addstr(row, column, '*')
+#         canvas.refresh()
+#         time.sleep(0.3)
+
+
+async def blink(canvas, row, column, symbol='*'):
     while True:
-        canvas.addstr(row, column, '*', curses.A_DIM)
-        canvas.refresh()
-        time.sleep(2)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        canvas.refresh()
-        time.sleep(0.5)
-        canvas.addstr(row, column, '*')
-        canvas.refresh()
-        time.sleep(0.3)
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
-    curses.wrapper(star)
+    curses.wrapper(draw)
 
