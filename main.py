88d00159244f6b87e31fb4738f4d8a21860621cf
sergryap <rs1180@mail.ru>
2022-12-23 18:27:14 +0500

    The star function has been created

--- main.py
+++ main.py
@@ -5,10 +5,31 @@ import curses
 def draw(canvas):
     row, column = (5, 20)
     canvas.addstr(row, column, 'Hello, World!')
+    canvas.border()
     canvas.refresh()
     time.sleep(2)
 
 
+def star(canvas):
+    curses.curs_set(False)
+    row, column = (5, 20)
+    canvas.border()
+    while True:
+        canvas.addstr(row, column, '*', curses.A_DIM)
+        canvas.refresh()
+        time.sleep(2)
+        canvas.addstr(row, column, '*')
+        canvas.refresh()
+        time.sleep(0.3)
+        canvas.addstr(row, column, '*', curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(0.5)
+        canvas.addstr(row, column, '*')
+        canvas.refresh()
+        time.sleep(0.3)
+
+
 if __name__ == '__main__':
     curses.update_lines_cols()
-    curses.wrapper(draw)
+    curses.wrapper(star)
+
