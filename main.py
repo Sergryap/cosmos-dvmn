954c0da32ecc7be548f32143d933371a7f4964de
sergryap <rs1180@mail.ru>
2022-12-25 20:24:13 +0500

    The blink func has been modified

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def draw(canvas):
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        d = random.randint(1, random.randint(10, 50))
+        d = random.randint(0, 5)
         for _ in range(d + 20):
             await asyncio.sleep(0)
 
